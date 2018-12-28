a9e2f16a36a40a1b99143484f9a777c1c9851b39
gajin7 <gajinjovan@gmail.com>
2018-12-28 18:19:07 +0100

    weapon improvements

---
+++ weapon.py
@@ -0,0 +1,11 @@
+from globals import *
+
+class Weapon(pygame.sprite.Sprite):
+    def __init__(self):
+        self.image = pygame.image.load('arrow.png')
+        self.rect = self.image.get_rect()
+        self.isActive = True
+        self.set_position()
+
+    def set_position(self, x=DISPLAY_WIDTH / 2, y=DISPLAY_HEIGHT+480):
+        self.rect.centerx, self.rect.bottom = x, y
